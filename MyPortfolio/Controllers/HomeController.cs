cf6207dcb71351510c2c5962988183dda6697555
azadgachayev <azad.gachayev@gmail.com>
2019-09-10 17:17:47 +0400

    myPortfolioMVC

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        PortfolioDB DB = new PortfolioDB();
+        public ActionResult Index()
+        {
+            ViewBag.Section1 = DB.Section1Div.Find(1);
+            ViewBag.workCat = DB.CategoryWorks.ToList();
+            ViewBag.workList = DB.MyWorksDivs.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
